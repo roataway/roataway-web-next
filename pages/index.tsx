2d6d494f7c5c5a4a7bcb098f492710f3ab4272c2
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-04-28 22:34:30 +0300

    feat: add openlayers map with vector tiles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,47 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Map as OlMap } from "ol";
+import { View } from "ol";
+import MapboxVector from "ol/layer/MapboxVector";
+import { Fragment, useEffect, useRef } from "react";
 
 const Home: NextPage = () => {
+	const mapRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (!mapRef.current) return;
+
+		const view = new View({
+			projection: "EPSG:3857",
+			center: [3209683.751386, 5946074.648818],
+			zoom: 13.5,
+			minZoom: 8,
+			maxZoom: 19,
+		});
+
+		const vectorLayer = new MapboxVector({
+			styleUrl: "https://maptiles.railean.net/styles/osm-bright/style.json",
+		});
+
+		new OlMap({
+			target: mapRef.current,
+			// Removes all default controls
+			controls: [],
+			view,
+			layers: [vectorLayer],
+		});
+	}, []);
+
 	return (
-		<div className="flex h-screen items-center justify-center">
+		<Fragment>
 			<Head>
 				<title>Roata Wăy</title>
 				<meta name="description" content="Transportul public din Chișinău" />
 				<link rel="icon" href="https://fav.farm/%F0%9F%9A%8E" />
 			</Head>
 
-			<main>
-				<h1>Coming soon...</h1>
-			</main>
-		</div>
+			<div ref={mapRef} className="h-screen" />
+		</Fragment>
 	);
 };
 
