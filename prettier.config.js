3f77556c5fbdcaea270ccbb8ed5b1cbe70d2600d
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-04-28 21:12:41 +0300

    chore: add tailwind prettier plugin

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	$schema: "http://json.schemastore.org/prettierrc",
+	useTabs: true,
+	tabWidth: 2,
+	quoteProps: "consistent",
+	trailingComma: "all",
+	plugins: [require("prettier-plugin-tailwindcss")],
+};
